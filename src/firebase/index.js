baa429c62169d561a9b1c6084495dc74bda5aa40
Mr. Qayum <omar.qayum@tdsb.on.ca>
2022-11-21 11:15:05 -0500

    Added Firebase support

---
+++ src/firebase/index.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getStorage, connectStorageEmulator } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBh9ujtOk-DfMvt5yIFLBVmAaxEWnpoAno",
+  authDomain: "ics3u-cadee.firebaseapp.com",
+  projectId: "ics3u-cadee",
+  storageBucket: "ics3u-cadee.appspot.com",
+  messagingSenderId: "580501545880",
+  appId: "1:580501545880:web:665528fb964e4d8b5ec1ed"
+};
+
+const auth = getAuth();
+const firestore = getFirestore();
+const storage = getStorage();
+
+connectAuthEmulator(auth, "http://localhost:9099");
+connectFirestoreEmulator(firestore, 'localhost', 8080);
+connectStorageEmulator(storage, "localhost", 9199);
+
+const app = initializeApp(firebaseConfig);
+
+export { auth, firestore, storage }
