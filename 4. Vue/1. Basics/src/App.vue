6689922b9ab52f4dd2e1ed0af442c9352b199fa4
Omar Qayum <omar.qayum@tdsb.on.ca>
2024-10-16 13:25:27 -0400

    Added Vue basics code

---
+++ 4. Vue/1. Basics/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Menu from "./components/Menu.vue"
+</script>
+
+<template>
+  <div class="menus">
+    <Menu :text="'Our Food Menu'" :menu="['Hot Dogs', 'Hamburgers', 'Pizza', 'Poutine', 'Fish']"></Menu>
+    <Menu :text="'Our Beverage Menu'" :menu="['Coke', '7-Up', 'Mountain Dew', 'Sprite', 'Ginger Ale', 'Dr. Pepper']"></Menu>
+  </div>
+</template>
+
+<style scoped>
+.menus {
+  display: flex;
+  gap: 3rem;
+}
+</style>
---
+++ 4. Vue/1. Basics/src/components/Menu.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps(['text', 'menu'])
+let showMenu = ref(true);
+
+function hideMenu() {
+    showMenu.value = !showMenu.value;
+}
+</script>
+
+<template>
+    <div>
+        <h1>{{ props.text }}</h1>
+        <ol v-if="showMenu">
+            <li v-for="item in props.menu">{{ item }}</li>
+        </ol>
+        <p v-else>Menu Hidden</p>
+        <button @click="hideMenu">Hide Menu</button>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+}
+
+ol li {
+    margin: 2rem;
+}
+</style>
