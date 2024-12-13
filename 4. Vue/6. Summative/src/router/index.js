e83ee69b6c0e22d73f59ed9e5fe78ce2210b14c2
Omar Qayum <omar.qayum@tdsb.on.ca>
2024-12-13 14:12:16 -0500

    Added error view

--- 4. Vue/6. Summative/src/router/index.js
+++ 4. Vue/6. Summative/src/router/index.js
@@ -6,20 +6,23 @@ import MoviesView from '../views/MoviesView.vue';
 import AllMoviesView from '../views/AllMoviesView.vue';
 import SingleMovieView from '../views/SingleMovieView.vue';
 import CartView from '../views/CartView.vue';
+import ErrorView from '../views/ErrorView.vue';
 
 const routes = [
-    { path: '/', component: HomeView },
-    { path: '/register', component: RegisterView },
-    { path: '/login', component: LoginView },
-    { path: '/cart', component: CartView },
+    { path: '/', meta: false, component: HomeView },
+    { path: '/register', meta: false, component: RegisterView },
+    { path: '/login', meta: false, component: LoginView },
+    { path: '/cart', meta: true, component: CartView },
     {
         path: '/movies',
         component: MoviesView,
+        meta: true,
         children: [
             { path: 'all', component: AllMoviesView },
             { path: ':id', component: SingleMovieView },
         ]
     },
+    { path: '/:pathMatch(.*)*', meta: false, name: 'NotFound', component: ErrorView, },
 ]
 
 const router = createRouter({
---
+++ 4. Vue/6. Summative/src/views/ErrorView.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <h1>This page does not exist</h1>
+</template>
+
+<style scope>
+h1 {
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: #e50914;
+}
+</style>
