5c2349ebc4161c881f469ff03463111079bd2b83
Omar Qayum <omar.qayum@tdsb.on.ca>
2024-12-13 14:37:24 -0500

    Added route guards and fixed refresh bug

--- 4. Vue/6. Summative/src/router/index.js
+++ 4. Vue/6. Summative/src/router/index.js
@@ -7,27 +7,40 @@ import AllMoviesView from '../views/AllMoviesView.vue';
 import SingleMovieView from '../views/SingleMovieView.vue';
 import CartView from '../views/CartView.vue';
 import ErrorView from '../views/ErrorView.vue';
+import { userAuthorized, useStore } from '../store';
 
 const routes = [
-    { path: '/', meta: false, component: HomeView },
-    { path: '/register', meta: false, component: RegisterView },
-    { path: '/login', meta: false, component: LoginView },
-    { path: '/cart', meta: true, component: CartView },
-    {
-        path: '/movies',
-        component: MoviesView,
-        meta: true,
-        children: [
-            { path: 'all', component: AllMoviesView },
-            { path: ':id', component: SingleMovieView },
-        ]
-    },
-    { path: '/:pathMatch(.*)*', meta: false, name: 'NotFound', component: ErrorView, },
+  { path: '/', meta: { auth: false }, component: HomeView },
+  { path: '/register', meta: { auth: false }, component: RegisterView },
+  { path: '/login', meta: { auth: false }, component: LoginView },
+  { path: '/cart', meta: { auth: true }, component: CartView },
+  {
+    path: '/movies',
+    component: MoviesView,
+    meta: { auth: true },
+    children: [
+      { path: 'all', component: AllMoviesView },
+      { path: ':id', component: SingleMovieView },
+    ]
+  },
+  { path: '/:pathMatch(.*)*', meta: { auth: false }, component: ErrorView, },
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 })
+
+router.beforeEach((to, from, next) => {
+  userAuthorized.then(() => {
+    const store = useStore();
+
+    if (!store.user && to.meta.auth) {
+      next("/login");
+    } else {
+      next();
+    }
+  });
+});
 
 export default router;
--- 4. Vue/6. Summative/src/store/index.js
+++ 4. Vue/6. Summative/src/store/index.js
@@ -1,9 +1,24 @@
 import { ref } from "vue"
 import { defineStore } from "pinia";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
 
 export const useStore = defineStore('store', () => {
   const user = ref(null);
   const cart = ref(new Map());
 
   return { user, cart }
+})
+
+export const userAuthorized = new Promise((resolve, reject) => {
+  onAuthStateChanged(auth, user => {
+    try {
+      const store = useStore();
+
+      store.user = user;
+      resolve();
+    } catch (error) {
+      reject();
+    }
+  })
 })
