7daf7e28e824c34bc41089831dbf6553431397c6
Qayum <Omar.Qayum@tdsb.on.ca>
2025-09-11 11:27:56 -0400

    Initial commit

---
+++ homework/QuadraticFormula.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class QuadraticFormula {
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please enter an a value:");
+        double a = keyboard.nextDouble();
+        System.out.println("Please enter a b value:");
+        double b = keyboard.nextDouble();
+        System.out.println("Please enter a c value:");
+        double c = keyboard.nextDouble();
+        keyboard.close();
+        
+        double discriminant = b * b - 4 * a * c;
+        double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        
+        System.out.println("Root 1 is: " + root1);
+        System.out.println("Root 2 is: " + root2);
+    }
+}
