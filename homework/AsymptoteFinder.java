cc5616e9d97489663380fb092c5fca539736ced8
Mr. Qayum <omar.qayum@tdsb.on.ca>
2026-02-25 14:52:12 -0500

    Added AsymptoteFinder code

---
+++ homework/AsymptoteFinder.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class AsymptoteFinder {
+
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please enter an m value:");
+        int m = keyboard.nextInt();
+        System.out.println("Please enter an n value:");
+        int n = keyboard.nextInt();
+
+        if (m == n) {
+            System.out.println("The asymptote is horizontal");
+        } else if (n > m) {
+            System.out.println("The asymptote is x-axis");
+        } else {
+            switch (m - n) {
+                case 1:
+                    System.out.println("The asymptote is linear");
+                    break;
+                case 2:
+                    System.out.println("The asymptote is qudratic");
+                    break;
+                case 3:
+                    System.out.println("The asymptote is cubic");
+                    break;
+                case 4:
+                    System.out.println("The asymptote is quartic");
+                    break;
+                case 5:
+                    System.out.println("The asymptote is quintic");
+                    break;
+                case 6:
+                    System.out.println("The asymptote is sextic");
+                    break;
+                case 7:
+                    System.out.println("The asymptote is heptic");
+                    break;
+                case 8:
+                    System.out.println("The asymptote is octic");
+                    break;
+                case 9:
+                    System.out.println("The asymptote is nonic");
+                    break;
+                case 10:
+                    System.out.println("The asymptote is decic");
+                    break;
+                default:
+                    System.out.println("Why so serious?");
+                    break;
+            }
+        }
+
+        keyboard.close();
+    }
+}
